35aebb4ba54240d1575a45137b8951ea6e2c9094
ChoJaeHwan <45838853+ChoJaeHwan@users.noreply.github.com>
2018-12-13 13:43:51 +0900

    깃처음깔고 테스트

---
+++ ConsoleApplication1/ConsoleApplication1/ddf.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "ddf.h"
+
+
+ddf::ddf()
+{
+	
+	ddf::a = 6;
+	ddf::d = 4;
+}
+
+
+ddf::~ddf()
+{
+	ddf::a = 0;
+	ddf::d = 0;
+}
---
+++ ConsoleApplication1/ConsoleApplication1/ddf.h
@@ -0,0 +1,12 @@
+#pragma once
+class ddf
+{
+public:
+
+	int a = 0;
+	int d = 0;
+
+	ddf();
+	~ddf();
+};
+

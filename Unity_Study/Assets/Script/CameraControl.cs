c340941ded55dfe7a625d6f4890bd45f0a126445
ChoJaeHwan <45838853+ChoJaeHwan@users.noreply.github.com>
2018-12-20 15:46:02 +0900

    Unity_AnimationPlayPrev

---
+++ Unity_Study/Assets/Script/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public float sensitivity = 700.0f;
+    float rotationX;
+    float rotationY;
+
+    void Update()
+    {
+        float mouseMoveValueX = Input.GetAxis("Mouse X");
+        float mouseMoveValueY = Input.GetAxis("Mouse Y");
+
+        rotationY += mouseMoveValueX * sensitivity * Time.deltaTime;
+        rotationX += mouseMoveValueY * sensitivity * Time.deltaTime;
+
+        rotationX %= 360; rotationY %= 360;
+
+        transform.eulerAngles = new Vector3(-rotationX, rotationY, 0.0f);
+    }
+}
---
+++ Unity_Study/Assets/Script/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator player=null;
+    CharacterController Character = null;
+    public Transform cameraTransform;
+    public float moveSpeed = 10.0f;
+
+    public float jumpSpeed = 10.0f;
+    public float gravity = -20.0f;
+
+   public float yVelocity = 0.0f;
+    // Use this for initialization
+    void Start () {
+        Character = GetComponent<CharacterController>();
+        player = GetComponent<Animator>();
+        if (!player)
+        {
+            Debug.Log("애니메이터가 없음");
+            return;
+        }
+          
+	}
+ 
+
+    void Update()
+    {
+        if(Input.GetKey("up")||Input.GetKey("down"))
+        {
+            player.Play("Run");
+        }
+        else if(Input.GetButton("Jump"))
+        {
+            player.Play("jump");
+
+        }
+        else
+        {
+            player.Play("Idle");
+
+        }
+        
+        float x = Input.GetAxis( "Horizontal" );
+        float z = Input.GetAxis( "Vertical" );
+
+        Vector3 moveDirection = new Vector3(x, 0, z);
+        moveDirection = cameraTransform.TransformDirection(moveDirection);
+        moveDirection *= moveSpeed;
+        if (Character.isGrounded == true)
+            yVelocity = 0.0f;
+
+        if (Input.GetButtonDown("Jump"))
+        { yVelocity = jumpSpeed; }
+
+        yVelocity += (gravity * Time.deltaTime);
+        moveDirection.y = yVelocity;
+        Character.Move(moveDirection * Time.deltaTime);
+    }
+}

c8927bc5fc2449bb0ce3afb449da40887bd9f043
ChoJaeHwan <45838853+ChoJaeHwan@users.noreply.github.com>
2018-12-21 15:17:46 +0900

    FSM

---
+++ Unity_Study/Assets/Script/FSM_NPC.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM_NPC : MonoBehaviour {
+
+
+    enum ENEMYSTATE
+    {
+
+        IDLE=0,
+        MOVE,
+        ATTACK,
+        DAMAGE,
+        DEAD
+
+    }
+    ENEMYSTATE enemyState = ENEMYSTATE.IDLE;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        switch (enemyState)
+        {
+            case ENEMYSTATE.IDLE:
+                {
+                }
+                break;
+            case ENEMYSTATE.MOVE:
+                {
+                }
+                break;
+            case ENEMYSTATE.ATTACK:
+                {
+                }
+                break;
+            case ENEMYSTATE.DAMAGE:
+                {
+                }
+                break;
+            case ENEMYSTATE.DEAD:
+                {
+                }
+                break;
+        }
+
+
+    }
+}
